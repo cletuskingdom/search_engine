a6c8d087d022a99e82cefa7cc8e3ba09fac3c613
Cletus Kingdom <cletuskingdom@gmail.com>
2023-08-02 16:37:57 +0100

    updated the controllers

--- src/app.js
+++ src/app.js
@@ -1,43 +1,21 @@
-require("dotenv").config();
+require("dotenv").config({ path: __dirname + "/../config/.env" });
 const express = require("express");
 const { Client } = require("@elastic/elasticsearch");
 const client = new Client({ node: "http://localhost:9200" }); // Replace with your ElasticSearch endpoint
 
 const app = express();
 const port = process.env.PORT || 2000;
+const path = require("path");
+// ...
+
+app.set("views", path.join(__dirname, "views"));
+
+// Set template engine
+app.set("view engine", "ejs");
 
 // middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-app.get("/search", async (req, res) => {
-	try {
-		const { q } = req.query;
-		console.log("Search query:", q);
-
-		const searchResult = await client.search({
-			index: "search",
-			body: {
-				query: {
-					match: {
-						content: q,
-					},
-				},
-			},
-		});
-		console.log("ElasticSearch result:", searchResult);
-
-		res.json(searchResult.body.hits.hits.map((hit) => hit._source));
-	} catch (err) {
-		console.error("Search error:", err);
-		res.status(500).json({
-			error: "An error occurred while searching.",
-		});
-	}
-});
-
-// Set template engine
-app.set("view engine", "ejs");
 
 // Route prefix - this points to all my routes
 app.use("", require("./routes/routes"));
